5f1847605566fd29248535d01e7dccdb5ae8bfc1
0xFFD800 <lukedgreene@gmail.com>
2021-03-27 07:29:19 -0500

    Redwall shrews no longer spawn in mossflower

--- src/main/java/com/bob/redwall/biomes/cool/BiomeRedwallForest.java
+++ src/main/java/com/bob/redwall/biomes/cool/BiomeRedwallForest.java
@@ -7,7 +7,6 @@ import com.bob.redwall.entity.npc.evil.EntityFerretMossflower;
 import com.bob.redwall.entity.npc.evil.EntityRatMossflower;
 import com.bob.redwall.entity.npc.evil.EntityWeaselMossflower;
 import com.bob.redwall.entity.npc.good.EntityOtterMossflower;
-import com.bob.redwall.entity.npc.good.redwall.EntityShrewRedwall;
 import com.bob.redwall.entity.npc.good.woodlander.EntityMoleWoodlander;
 import com.bob.redwall.entity.npc.good.woodlander.EntityMouseWoodlander;
 import com.bob.redwall.entity.npc.good.woodlander.EntitySquirrelWoodlander;
@@ -42,7 +41,6 @@ public class BiomeRedwallForest extends Biome {
 	        this.spawnableCreatureList.add(new SpawnListEntry(EntityMouseWoodlander.class, 1, 3, 6));
 	        this.spawnableCreatureList.add(new SpawnListEntry(EntityOtterMossflower.class, 1, 1, 7));
 	        this.spawnableCreatureList.add(new SpawnListEntry(EntitySquirrelWoodlander.class, 1, 1, 4));
-	        this.spawnableCreatureList.add(new SpawnListEntry(EntityShrewRedwall.class, 1, 1, 4));
         }
 
         if (this.type == BiomeRedwallForest.Type.FLOWER) {
